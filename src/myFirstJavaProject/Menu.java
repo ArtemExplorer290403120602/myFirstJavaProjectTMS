bb0e08c53083d0359ca0db8ccea64b4b30c5f64b
User <artem.zakrzhevski@mail.ru>
2023-12-26 06:49:39 +0300

    Change class Menu

--- src/myFirstJavaProject/Menu.java
+++ src/myFirstJavaProject/Menu.java
@@ -5,32 +5,37 @@ import actions.TransferMoneyToAnAccount;
 import actions.ViewingTheTotal;
 
 import java.io.*;
-import java.util.Scanner;
 
 public class Menu extends TransferMoneyToAnAccount {
     protected static void menu() {
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(REPORT_FILE));
+        boolean exit = false;
+        while (!exit) {
+            try {
+                BufferedReader reader = new BufferedReader(new FileReader(REPORT_FILE));
 
-            // Вывод списка всех переводов из файла-отчета
-            String line;
-            while ((line = reader.readLine()) != null) {
-                System.out.println(line);
-            }
+                // Вывод списка всех переводов из файла-отчета
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    System.out.println(line);
+                }
+                reader.close();
 
-            reader.close();
-
-            System.out.print(""" 
-                    Добро пожаловать в банковскую систему , перед вам существуют вот такие дейсвтия:
+                System.out.println("""
+                    Добро пожаловать в банковскую систему. Перед вами доступны следующие действия:
                       1: Парсинг файлов перевода из input
-                      2: Вывод списка всех переводов из файла-отчета.
-                      """);
-            System.out.print("Пожалуйста введите выбранную вами цифру: ");
-            BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
-            int action = Integer.parseInt(consoleReader.readLine());
+                      2: Вывод списка всех переводов из файла-отчета
+                      0: Завершить программу
+                    """);
+                System.out.print("Пожалуйста, введите выбранную вами цифру: ");
+                BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
+                int action = Integer.parseInt(consoleReader.readLine());
 
                 switch (action) {
-                    case 1: ParsingMoney.parseFiles();
+                    case 0:
+                        exit = true;
+                        break;
+                    case 1:
+                        ParsingMoney.parseFiles();
                         break;
                     case 2:
                         ViewingTheTotal.showReport();
@@ -38,11 +43,14 @@ public class Menu extends TransferMoneyToAnAccount {
                     default:
                         System.out.println("Некорректный выбор действия");
                         break;
+                }
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } catch (NumberFormatException e) {
+                System.out.println("Ошибка ввода. Пожалуйста, введите числовое значение.");
             }
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
     }
 }
